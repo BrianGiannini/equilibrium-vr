a0987bdd74a70e0aabb1a002c1abdfbd75f893f3
Brian Giannini <brian.giannini.1@gmail.com>
2019-08-03 18:20:36 +0200

    Add models + animations + simple AI script

---
+++ Assets/SimpleCorridor/Scripts/Pause.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void pauseGame()
+    {
+        Debug.Log("entered");
+        Time.timeScale = 0.05f;
+    }
+
+    public void unPauseGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        pauseGame();
+    }
+
+
+   
+}
