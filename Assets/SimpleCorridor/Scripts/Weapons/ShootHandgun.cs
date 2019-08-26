4b33fea93194f06f39375a7d248da151e0edfcf0
Brian Giannini <brian.giannini.1@gmail.com>
2019-08-26 23:36:19 +0200

    add weapon with bullet and animations - clean stuff

---
+++ Assets/SimpleCorridor/Scripts/Weapons/ShootHandgun.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class ShootHandgun : MonoBehaviour
+
+{
+    private bool isReloading = false;
+    public SteamVR_Action_Boolean fireAction = null;
+    private Animator animator = null;
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Pose = GetComponentInParent<SteamVR_Behaviour_Pose>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (fireAction.GetStateDown(m_Pose.inputSource))
+        {
+            animator.SetBool("Fire", true);
+            Fire();
+        }
+
+       
+
+    }
+
+    private void Fire()
+    {
+        audioSource.PlayOneShot(audioClip, 0.5F);            
+     }
+}
