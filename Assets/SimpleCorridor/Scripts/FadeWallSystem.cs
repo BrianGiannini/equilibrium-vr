e6f51cffad7b75433a3c16a2a8c28d63614d5331
Brian Giannini <brian.giannini.1@gmail.com>
2019-08-28 00:25:47 +0200

    add fade wall system

---
+++ Assets/SimpleCorridor/Scripts/FadeWallSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class FadeWallSystem : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("HeadCollider"))
+        {
+            SteamVR_Fade.Start(Color.black, 1f);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("HeadCollider"))
+        {
+            SteamVR_Fade.Start(Color.clear, 1f);
+        }
+    }
+}
